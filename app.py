55ec1bc7b21df138cbfbb77e7aaa475fdbed6213
Sherin Thomas <sherinmt@gmail.com>
2021-09-08 20:14:08 +1000

    app and crimedata changes

--- app.py
+++ app.py
@@ -9,10 +9,10 @@ app = Flask(__name__)
 
 # Use PyMongo to establish Mongo connection
 client = pymongo.MongoClient("mongodb://localhost:27017")
-database = client['crime_database']
-crime_collection = database.get_collection("crime_table")
-offence_collection = database.get_collection("offencetable")
-victim_collection = database.get_collection("victimtable")
+database = client['crimedataDB']
+crime_collection = database.get_collection("crimeDB")
+offence_collection = database.get_collection("offenceDB")
+victim_collection = database.get_collection("victimDB")
 
 #Route /
 @app.route("/")
@@ -24,13 +24,39 @@ def crime_incidents(year):
 
     """Fetch the criminal incidents data for specific year"""
     crime_records = crime_collection.find(
-        {"Year" : int(year)}, {'_id':0})
+        {"year" : int(year)}, {'_id':0})
 
     crime_list = list(crime_records)
     # Converting to the JSON
     crime_json_data = dumps(crime_list, indent = 2) 
-
+    print(crime_json_data)
     return crime_json_data
+
+@app.route("/offence_type/<year>")
+def offence_type(year):
+
+    """Fetch the criminal incidents data for specific year"""
+    offence_records = offence_collection.find(
+        {"year" : int(year)}, {'_id':0})
+
+    offence_list = list(offence_records)
+    # Converting to the JSON
+    offence_json_data = dumps(offence_list, indent = 2) 
+    print(offence_json_data)
+    return offence_json_data
+
+@app.route("/victim_data/<year>")
+def victim_data(year):
+
+    """Fetch the criminal incidents data for specific year"""
+    victim_records = victim_collection.find(
+        {"year" : int(year)}, {'_id':0})
+
+    victim_list = list(victim_records)
+    # Converting to the JSON
+    victim_json_data = dumps(victim_list, indent = 2) 
+    print(victim_json_data)
+    return victim_json_data
 
 if __name__ == '__main__':
     app.run(debug=True)
