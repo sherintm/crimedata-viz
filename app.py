4d2c06d5b8fcc30e278a4b092e09f3c81f3e0c5b
Sherin Thomas <sherinmt@gmail.com>
2021-09-09 23:26:17 +1000

    Added api endpoints in app.py

--- app.py
+++ app.py
@@ -1,25 +1,43 @@
-from flask import Flask, render_template, redirect
-from flask.json import jsonify
+from flask import Flask
 from flask_pymongo import pymongo
-from bson.json_util import dumps, loads
-
+from bson.json_util import dumps
+from flask_cors import CORS, cross_origin
 
 # Create an instance of Flask
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 # Use PyMongo to establish Mongo connection
 client = pymongo.MongoClient("mongodb://localhost:27017")
-database = client['crimedataDB']
-crime_collection = database.get_collection("crimeDB")
-offence_collection = database.get_collection("offenceDB")
-victim_collection = database.get_collection("victimDB")
+database = client['crime_database']
+crime_collection = database.get_collection("crime_table")
+lga_data_collection = database.get_collection("lga_data_table")
+offence_collection = database.get_collection("offence_table")
+victim_collection = database.get_collection("victim_table")
 
 #Route /
 @app.route("/")
+@cross_origin()
 def home():
     return "Home"
 
+@app.route("/crime_incidents")
+@cross_origin()
+def all_crime_incidents():
+
+    """Fetch the criminal incidents data for specific year"""
+    crime_records = crime_collection.find(
+        {}, {'_id':0})
+
+    crime_list = list(crime_records)
+    # Converting to the JSON
+    crime_json_data = dumps(crime_list, indent = 2) 
+    print(crime_json_data)
+    return crime_json_data
+
 @app.route("/crime_incidents/<year>")
+@cross_origin()
 def crime_incidents(year):
 
     """Fetch the criminal incidents data for specific year"""
@@ -32,7 +50,22 @@ def crime_incidents(year):
     print(crime_json_data)
     return crime_json_data
 
+@app.route("/offence_type")
+@cross_origin()
+def all_offence_type():
+
+    """Fetch the criminal incidents data for specific year"""
+    offence_records = offence_collection.find(
+        {}, {'_id':0})
+
+    offence_list = list(offence_records)
+    # Converting to the JSON
+    offence_json_data = dumps(offence_list, indent = 2) 
+    print(offence_json_data)
+    return offence_json_data
+
 @app.route("/offence_type/<year>")
+@cross_origin()
 def offence_type(year):
 
     """Fetch the criminal incidents data for specific year"""
@@ -45,7 +78,22 @@ def offence_type(year):
     print(offence_json_data)
     return offence_json_data
 
+@app.route("/victim_data")
+@cross_origin()
+def all_victim_data():
+
+    """Fetch the criminal incidents data for specific year"""
+    victim_records = victim_collection.find(
+        {}, {'_id':0})
+
+    victim_list = list(victim_records)
+    # Converting to the JSON
+    victim_json_data = dumps(victim_list, indent = 2) 
+    print(victim_json_data)
+    return victim_json_data
+
 @app.route("/victim_data/<year>")
+@cross_origin()
 def victim_data(year):
 
     """Fetch the criminal incidents data for specific year"""
@@ -57,6 +105,41 @@ def victim_data(year):
     victim_json_data = dumps(victim_list, indent = 2) 
     print(victim_json_data)
     return victim_json_data
+
+@app.route("/lga_crime_data/<year>")
+@cross_origin()
+def lga_crime_data(year):
+
+    lga_data_records = lga_data_collection.find(
+    {}, {'_id':0})
+
+    crime_records = crime_collection.find(
+    {"year" : int(year)}, {'_id':0})
+
+    lga_data_list = list(lga_data_records)
+
+
+    for crime_data in crime_records:
+        i = 0
+
+        for lga in lga_data_list:
+            #print(lga)
+            lga_crime_data = {}
+            if(lga['properties']['vic_lga__3'] == crime_data['lga'].upper()):
+                lga_crime_data['year'] = crime_data['year']
+                lga_crime_data['incidents_recorded'] = crime_data['incidents_recorded']
+                lga_crime_data['rate_per_100k'] = crime_data['rate_per_100k_population']
+                lga_data_list[i]['properties']['lga_crime_data'] = lga_crime_data
+                print(lga_crime_data)
+                print(lga['properties']['vic_lga__3'])
+                print(i)
+            i += 1
+
+    # Converting to the JSON
+    lga_crime_json_data = dumps(lga_data_list, indent = 2)
+
+    return lga_crime_json_data
+
 
 if __name__ == '__main__':
     app.run(debug=True)
